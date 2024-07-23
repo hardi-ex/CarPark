a4ca973eac27d7f004e70959eb6eef6209b3ecb2
Yaroslav <skylandtat@ukr.net>
2024-07-23 21:10:43 +0200

    edit Advert

--- src/components/Advert/Advert.jsx
+++ src/components/Advert/Advert.jsx
@@ -5,32 +5,31 @@ export const Advert = ({ advert }) => {
     advert;
 
   return (
-    <>
+    <div className={css.card}>
       <div className={css.imageWrapper}>
         <img src={img} alt={make} className={css.image} />
         <div className={css.heartIcon}>❤️</div>
       </div>
       <div className={css.details}>
         <h4 className={css.title}>
-          {make}&nbsp;
-          {model},&nbsp;{year}
+          {make}&nbsp;{model},&nbsp;{year}
         </h4>
         <p className={css.price}>{rentalPrice}</p>
         <p className={css.info}>
-          <span className={css.spanInfo}>Address: </span>
-          {address}
+          <span className={css.spanInfo}>Type: </span>
+          {type}
         </p>
         <p className={css.info}>
           <span className={css.spanInfo}>Rental company: </span>
           {rentalCompany}
         </p>
         <p className={css.info}>
-          <span className={css.spanInfo}>Type: </span>
-          {type}
+          <span className={css.spanInfo}>Address: </span>
+          {address}
         </p>
       </div>
       <button className={css.learnMoreButton}>Learn more</button>
-    </>
+    </div>
   );
 };
 
