0324cda09dbbd581a624e83b213125e455eeaace
Yaroslav <skylandtat@ukr.net>
2024-07-24 23:57:16 +0200

    Fix Modal v2 (Adaptive css)

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,6 +3,7 @@ ReactModal.setAppElement("#root");
 import css from "./Modal.module.css";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
+import { useEffect } from "react";
 
 const Modal = ({ isOpen, onRequestClose, advert }) => {
   const {
@@ -23,101 +24,100 @@ const Modal = ({ isOpen, onRequestClose, advert }) => {
     functionalities,
   } = advert;
 
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [isOpen]);
+
   return (
     <ReactModal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
       contentLabel="Advert Details"
+      className={css.modalContent}
+      overlayClassName={css.ReactModal__Overlay}
       style={{
-        content: {
-          top: "50%",
-          left: "50%",
-          right: "auto",
-          bottom: "auto",
-          marginRight: "-50%",
-          transform: "translate(-50%, -50%)",
-          width: "550px",
-          minHeight: "650px",
-          padding: "30px",
-          borderRadius: "20px",
-        },
         overlay: {
           backgroundColor: "rgba(0, 0, 0, 0.5)",
         },
       }}
     >
-      <div className={css.modalContent}>
-        <button className={css.closeButton} onClick={onRequestClose}>
-          &times;
-        </button>
+      <button className={css.closeButton} onClick={onRequestClose}>
+        &times;
+      </button>
 
-        <div className={css.imageWrapper}>
-          <LazyLoadImage
-            className={css.imgCar}
-            src={img}
-            alt={make}
-            effect="blur"
-          />
-        </div>
-        <h2 className={css.title}>
-          {make}&nbsp;{model},&nbsp;{year}
-        </h2>
-
-        <div className={css.mainWrap}>
-          <p className={css.text}>
-            <span className={css.mainText}>Rental Price: </span>
-            {rentalPrice}
-          </p>
-          <p className={css.text}>
-            <span className={css.mainText}>Type: </span>
-            {type}
-          </p>
-          <p className={css.text}>
-            <span className={css.mainText}>Engine size: </span>
-            {engineSize}
-          </p>
-          <p className={css.text}>
-            <span className={css.mainText}>Fuel consumption: </span>
-            {fuelConsumption}
-          </p>
-          <p className={css.text}>
-            <span className={css.mainText}>Mileage: </span>
-            {mileage}
-          </p>
-          <p className={css.text}>
-            <span className={css.mainText}>Description: </span>
-            {description}
-          </p>
-          <p className={css.text}>
-            <span className={css.mainText}>Rental conditions: </span>
-            {rentalConditions}
-          </p>
-        </div>
-
-        <div className={css.additionalWrap}>
-          <h3 className={css.titleAdditional}>
-            Accessories and functionalities:{" "}
-          </h3>
-          <p className={css.text}>{accessories.join(" | ")}</p>
-          <p className={css.text}>{functionalities.join(" | ")}</p>
-        </div>
-
-        <div className={css.additionalWrap}>
-          <h3 className={css.titleAdditional}>Rental Information</h3>
-          <p className={css.text}>
-            <span className={css.mainText}>Rental Company: </span>{" "}
-            {rentalCompany}
-          </p>
-          <p className={css.text}>
-            <span className={css.mainText}>Address: </span>
-            {address}
-          </p>
-        </div>
-
-        <a className={css.btnRent} href="tel:+380730000000">
-          Rental car <img src="/phone.svg" alt="Phone" width="20" height="20" />
-        </a>
+      <div className={css.imageWrapper}>
+        <LazyLoadImage
+          className={css.imgCar}
+          src={img}
+          alt={make}
+          effect="blur"
+        />
       </div>
+      <h2 className={css.title}>
+        {make}&nbsp;{model},&nbsp;{year}
+      </h2>
+
+      <div className={css.mainWrap}>
+        <p className={css.text}>
+          <span className={css.mainText}>Rental Price: </span>
+          {rentalPrice}
+        </p>
+        <p className={css.text}>
+          <span className={css.mainText}>Type: </span>
+          {type}
+        </p>
+        <p className={css.text}>
+          <span className={css.mainText}>Engine size: </span>
+          {engineSize}
+        </p>
+        <p className={css.text}>
+          <span className={css.mainText}>Fuel consumption: </span>
+          {fuelConsumption}
+        </p>
+        <p className={css.text}>
+          <span className={css.mainText}>Mileage: </span>
+          {mileage}
+        </p>
+        <p className={css.text}>
+          <span className={css.mainText}>Description: </span>
+          {description}
+        </p>
+        <p className={css.text}>
+          <span className={css.mainText}>Rental conditions: </span>
+          {rentalConditions}
+        </p>
+      </div>
+
+      <div className={css.additionalWrap}>
+        <h3 className={css.titleAdditional}>
+          Accessories and functionalities:{" "}
+        </h3>
+        <p className={css.text}>{accessories.join(" | ")}</p>
+        <p className={css.text}>{functionalities.join(" | ")}</p>
+      </div>
+
+      <div className={css.additionalWrap}>
+        <h3 className={css.titleAdditional}>Rental Information</h3>
+        <p className={css.text}>
+          <span className={css.mainText}>Rental Company: </span> {rentalCompany}
+        </p>
+        <p className={css.text}>
+          <span className={css.mainText}>Address: </span>
+          {address}
+        </p>
+      </div>
+
+      <a className={css.btnRent} href="tel:+380730000000">
+        Rental car <img src="/phone.svg" alt="Phone" width="20" height="20" />
+      </a>
     </ReactModal>
   );
 };
