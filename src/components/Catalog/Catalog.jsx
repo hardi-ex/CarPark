2cba92483c9ce96f42caed976c3c07ffa089c59b
Yaroslav <skylandtat@ukr.net>
2024-07-27 13:37:24 +0200

    Add btn filters

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -1,18 +1,31 @@
 import { useState } from "react";
+import { useTranslation } from "react-i18next";
 import AdvertList from "../AdvertList/AdvertList";
 import SearchBox from "../SearchBox/SearchBox";
 import css from "./Catalog.module.css";
 
 export const Catalog = () => {
   const [filters, setFilters] = useState({});
+  const [showFilters, setShowFilters] = useState(false);
+
+  const { t } = useTranslation();
 
   const handleSearch = (newFilters) => {
     setFilters(newFilters);
   };
 
+  const toggleFilters = () => {
+    setShowFilters(!showFilters);
+  };
+
   return (
     <div className={css.catalog}>
-      <SearchBox onSearch={handleSearch} />
+      <button className={css.filtersButton} onClick={toggleFilters}>
+        {showFilters ? t("hideFilters") : t("showFilters")}
+      </button>
+      <div className={`${css.searchBox} ${showFilters ? css.show : css.hide}`}>
+        <SearchBox onSearch={handleSearch} />
+      </div>
       <AdvertList filters={filters} />
     </div>
   );
