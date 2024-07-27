2167dce2ea573166de0bf6a932456841d9a2a7c0
Yaroslav <skylandtat@ukr.net>
2024-07-27 21:23:10 +0200

    Add description of operation

--- src/redux/catalog/operations.js
+++ src/redux/catalog/operations.js
@@ -14,3 +14,7 @@ export const getAllAdverts = createAsyncThunk(
     }
   }
 );
+
+// З огляду на те, що mockapi.io не надає широких можливостей для фільтрації, ми використовуємо один запит для отримання всіх оголошень, а фільтрацію виконуємо вже на клієнтській стороні. Також немає сенсу використовувати пагінацію, оскільки в будь-якому разі доведеться робити один загальний запит. Цей метод має право на існування лише при невеликій кількості даних з бекенду, як і у нашому випадку.
+
+// Considering that mockapi.io does not provide extensive filtering capabilities, we use a single request to fetch all advertisements and perform filtering on the client side. There is also no point in implementing pagination since we still have to make a single general request. This method is only viable with a small amount of data from the backend, as is the case with our current situation.
